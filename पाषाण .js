b080f082d34090a67dbf0167d432eeecd4630fa2
Queen-Vaibhavi <77841320+Queen-Vaibhavi@users.noreply.github.com>
2021-06-02 20:58:32 +0530

    Update पाषाण .js

--- पाषाण .js
+++ पाषाण .js
@@ -1,29 +1,34 @@
-class Pebble{
-    constructor(bodyA, pointBo){
-        var options = {
-            bodyA: bodyA,
-            pointB: pointBo,
-            stiffness: 0.004,
-            length: 20
-        }
-        this.stone = Constraint.create(options);
-        World.add(world, this.stone);
-        this.pointB = pointBo;
-    }
+class stone2
+{
+	constructor(x,y,r)
+	{
+		var options={
+			isStatic:false,
+			restitution:0,
+			friction:1,
+			density:1.2
+			}
+		this.x=x;
+		this.y=y;
+		this.r=r
+		this.image=loadImage("images/stone.png");
+		this.body=Bodies.circle(this.x, this.y, this.r/2, options)
+		World.add(world, this.body);
 
-    display(){
-        if(this.stone.bodyA){
-        var pointA = this.stone.bodyA.position;
-        var pointB = this.pointB;
-        strokeWeight(4);
-        line(pointA.x, pointA.y, pointB.x, pointB.y);
-    
-        }
+	}
+	display()
+	{
+			var stone2pos=this.body.position;		
+			push()
+			translate(stone2pos.x, stone2pos.y);
+			// rectMode(CENTER)
+			// rotate(this.body.angle)
+			fill(255,0,255)
+			imageMode(CENTER);
+			ellipseMode(RADIUS)
+			image(this.image, 0,0,this.r*2, this.r*2)
+			pop()
+			
+	}
 
-        }
-    fly(){
-    this.stone.bodyA = null;
-    
-    }
-    
 }
